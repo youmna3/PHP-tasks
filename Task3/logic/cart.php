cee492f74da40e4e2c9bb6382a22c93fbc8ac576
youmna3 <youmna54@gmail.com>
2023-01-10 15:28:37 +0200

    task 3

---
+++ Task3/logic/cart.php
@@ -0,0 +1,29 @@
+<?php
+require_once(BASE_PATH . 'dal/dal.php');
+function addProductToCart($product)
+{
+    if (session_status() === PHP_SESSION_NONE)
+        session_start();
+
+    $cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
+    $found = false;
+    for ($i = 0; $i < count($cart); $i++) {
+        if ($cart[$i]['product']['id'] === $product['id']) {
+            $cart[$i]['quantity'] += 1;
+            $found = true;
+        }
+    }
+    if (!$found) {
+        array_push($cart, ['product' => $product, 'quantity' => 1]);
+    }
+    $_SESSION['cart'] = $cart;
+}
+
+function getCart()
+{
+    if (session_status() === PHP_SESSION_NONE)
+        session_start();
+
+    $cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
+    return $cart;
+}
