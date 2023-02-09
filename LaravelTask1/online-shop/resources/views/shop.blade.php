245480fe282d7e00edb4bccbd7a4d9e6864fc5e5
youmna3 <youmna54@gmail.com>
2023-02-09 21:16:00 +0200

    add links to shop breadcrumb

--- LaravelTask1/online-shop/resources/views/shop.blade.php
+++ LaravelTask1/online-shop/resources/views/shop.blade.php
@@ -16,8 +16,8 @@
         <div class="row px-xl-5">
             <div class="col-12">
                 <nav class="breadcrumb bg-light mb-30">
-                    <a class="breadcrumb-item text-dark" href="#">Home</a>
-                    <a class="breadcrumb-item text-dark" href="#">Shop</a>
+                    <a class="breadcrumb-item text-dark" href="{{ url('/') }}">Home</a>
+                    <a class="breadcrumb-item text-dark" href="{{ url('/shop') }}">Shop</a>
                     <span class="breadcrumb-item active">Shop List</span>
                 </nav>
             </div>
